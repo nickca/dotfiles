e3014b6b9ce953205c30eb870fe8225d20f6f914
Nick Caccamisi <nickca@gmail.com>
2023-01-06 02:04:02 -0600

    conky

--- rofi/applets/applets/powermenu.sh
+++ rofi/applets/applets/powermenu.sh
@@ -10,10 +10,6 @@ style="$($HOME/.config/rofi/applets/applets/style.sh)"
 dir="$HOME/.config/rofi/applets/applets/configs/$style"
 rofi_command="rofi -theme $dir/powermenu.rasi"
 
-uptime=$(uptime -p | sed -e 's/up //g')
-cpu=$($HOME/.config/rofi/bin/usedcpu)
-memory=$($HOME/.config/rofi/bin/usedram)
-
 # Options
 shutdown=""
 reboot=""
@@ -38,7 +34,7 @@ msg() {
 # Variable passed to rofi
 options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 
-chosen="$(echo -e "$options" | $rofi_command -p "Uptime - $uptime" -dmenu -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -p "Shutdown/Logoff" -dmenu -selected-row 2)"
 case $chosen in
     $shutdown)
 		ans=$(confirm_exit &)
@@ -67,7 +63,8 @@ case $chosen in
 		#	betterlockscreen -l
 		#fi
         #;;
-        xscreensaver-command -lock
+        #xscreensaver-command -lock
+	~/screensaver
         ;;
     $suspend)
 		ans=$(confirm_exit &)
